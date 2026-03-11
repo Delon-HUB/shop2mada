885fc40a2f9b8590d0b81e99ce9c5f484703e391
Delon <nicolas123delon@gmail.com>
2026-03-11 19:04:27 +0300

    feat: shopping cart component

---
+++ frontend/src/components/ShoppingCart.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-card flat bordered class="fit">
+    <q-card-section class="q-pt-none" style="position: sticky; top: 0">
+      <q-item class="q-pa-none">
+        <q-item-section>
+          <q-item-label class="text-bold text-h6"
+            ><q-icon name="shopping_cart" color="blue" />Panier</q-item-label
+          >
+        </q-item-section>
+        <q-space />
+        <q-item-section>
+          <q-item-label class="text-right"
+            ><q-btn flat icon-right="delete" color="red"
+          /></q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator />
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <q-list bordered>
+        <q-item v-for="i in 20" clickable v-ripple>
+          <q-item-section>Single line item</q-item-section>
+        </q-item>
+      </q-list>
+    </q-card-section>
+    <q-card-actions align="center" style="position: absolute; bottom: 0; left: 0; right: 0">
+      <q-btn no-caps color="blue" class="fit">Acheter</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+<script setup lang="ts"></script>
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,34 +1,7 @@
 <template>
   <q-layout view="hHr lpR lff" container>
     <q-drawer side="right" v-model="drawer" :width="500" :breakpoint="200">
-      <q-card flat bordered class="fit">
-        <q-card-section class="q-pt-none" style="position: sticky; top: 0">
-          <q-item class="q-pa-none">
-            <q-item-section>
-              <q-item-label class="text-bold text-h6"
-                ><q-icon name="shopping_cart" color="blue" />Panier</q-item-label
-              >
-            </q-item-section>
-            <q-space />
-            <q-item-section>
-              <q-item-label class="text-right"
-                ><q-btn flat icon-right="delete" color="red"
-              /></q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-separator />
-        </q-card-section>
-        <q-card-section class="q-pt-none">
-          <q-list bordered>
-            <q-item v-for="i in 20" clickable v-ripple>
-              <q-item-section>Single line item</q-item-section>
-            </q-item>
-          </q-list>
-        </q-card-section>
-        <q-card-actions align="center" style="position: absolute; bottom: 0; left: 0; right: 0">
-          <q-btn no-caps color="blue" class="fit">Acheter</q-btn>
-        </q-card-actions>
-      </q-card>
+      <shopping-cart />
     </q-drawer>
 
     <q-header class="header">
@@ -101,6 +74,7 @@
   </q-layout>
 </template>
 <script setup lang="ts">
+import ShoppingCart from '@/components/ShoppingCart.vue'
 import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 const covers = ref(1)
