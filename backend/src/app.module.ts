5477080f1c3503e6bd39a95c575bb5793a02ddd0
Delon <nicolas123delon@gmail.com>
2026-04-15 10:47:08 +0300

    chore: install mongoose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.DB_URI ?? 'mongodb://localhost:27017/shop2mada',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
