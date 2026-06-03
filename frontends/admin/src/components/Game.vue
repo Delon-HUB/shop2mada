26b7bc3e81b3460f7ad54041248695890144b1c1
Delon <nicolas123delon@gmail.com>
2026-06-03 13:31:28 +0300

    feat(admin): design game page

--- frontends/admin/src/components/Game.vue
+++ frontends/admin/src/components/Game.vue
@@ -1,17 +1,91 @@
 <template>
-  <q-card flat bordered>
-    <q-splitter v-model="splitterModel">
-      <template v-slot:before>
-        <div>
-          <q-img class="image" :src="props.game.cover" />
-          <p class="text-center text-h6 text-bold q-ma-none">{{ props.game.name }}</p>
-          <p class="text-center text-subtitle2 text-grey q-ma-none">
-            {{ props.game.categories.map((categ) => categ).join(', ') }}
+  <q-card flat bordered class="q-mt-none" :class="$q.screen.gt.md ? 'row' : ''">
+    <div :class="$q.screen.gt.md ? 'col-3 full-height' : ''">
+      <q-card-section class="q-pa-none q-ma-none">
+        <q-img class="image" :src="props.game.cover" :ratio="16 / 9">
+          <p class="text-right">
+            <q-chip color="dark" class="text-bold text-white text-h6">{{ props.game.name }}</q-chip>
           </p>
-        </div>
-      </template>
+        </q-img>
 
-      <template v-slot:after>
+        <p
+          style="transform: translateY(-100%)"
+          class="q-pa-none q-ma-none absolute full-width text-right"
+          v-if="$q.screen.lt.md"
+        >
+          <q-btn-dropdown
+            flat
+            round
+            color="primary"
+            dropdown-icon="settings"
+            class="text-white bg-dark"
+          >
+            <q-list>
+              <q-item clickable>
+                <q-item-section>
+                  <q-item-label><q-icon size="24px" name="edit" />Modifier </q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable>
+                <q-item-section>
+                  <q-item-label
+                    ><q-icon size="24px" color="red" name="delete" />Supprimer
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable @click="() => (offerDialog = true)">
+                <q-item-section>
+                  <q-item-label
+                    ><q-icon size="24px" color="primary" name="add" />Nouvelle offre
+                  </q-item-label>
+                </q-item-section>
+                <offer-input-dialog
+                  :game="props.game"
+                  v-model="offerDialog"
+                  @finished="(offer: Partial<IOffer>) => handleAddOffer(offer)"
+                />
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </p>
+
+        <div class="flex row wrap flex-start" v-if="$q.screen.gt.md">
+          <q-item clickable>
+            <q-item-section>
+              <q-item-label><q-icon size="24px" name="edit" />Modifier </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable>
+            <q-item-section>
+              <q-item-label
+                ><q-icon size="24px" color="red" name="delete" />Supprimer
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable @click="() => (offerDialog = true)">
+            <q-item-section>
+              <q-item-label
+                ><q-icon size="24px" color="primary" name="add" />Nouvelle offre
+              </q-item-label>
+            </q-item-section>
+            <offer-input-dialog
+              :game="props.game"
+              v-model="offerDialog"
+              @finished="(offer: Partial<IOffer>) => handleAddOffer(offer)"
+            />
+          </q-item>
+        </div>
+      </q-card-section>
+    </div>
+
+    <q-separator vertical></q-separator>
+
+    <div :class="$q.screen.gt.md ? 'col' : ''">
+      <q-card-section class="q-pa-none q-ma-none">
         <q-tabs
           v-model="offerTab"
           dense
@@ -20,29 +94,47 @@
           indicator-color="primary"
           align="justify"
         >
-          <q-tab
+          <q-btn-dropdown
             no-caps
             v-for="offer in props.game.offers"
             :key="offer.id"
-            :name="offer.id"
+            @click="() => (offerTab = offer.id)"
+            split
+            flat
             :label="offer.name"
-          />
+            class="q-ma-sm"
+            :color="offerTab == offer.id ? 'primary' : 'grey'"
+          >
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Renommer</q-item-label>
+                </q-item-section>
+              </q-item>
 
-          <q-tab name="add" icon="add" no-caps @click="() => (showOfferInput = true)">
-            <offer-input-dialog
-              :game="props.game"
-              v-model="showOfferInput"
-              @finished="(offer: Partial<IOffer>) => handleAddOffer(offer)"
-            />
-          </q-tab>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>Supprimer</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
         </q-tabs>
+      </q-card-section>
 
+      <q-separator />
+
+      <q-card-section class="q-pa-none q-ma-none">
         <q-tab-panels v-model="offerTab" animated>
           <q-tab-panel
             v-for="offer in props.game.offers"
             :key="offer.id"
             :name="offer.id"
-            class="flex row wrap"
+            :class="
+              $q.screen.lt.md
+                ? 'flex row wrap q-pa-none justify-around'
+                : ' flex row wrap q-pa-none flex-start'
+            "
           >
             <article-item
               v-for="article in offer.articles"
@@ -51,27 +143,27 @@
               class="q-ma-sm"
             />
 
-            <q-card flat bordered class="q-ma-md article text-center">
+            <p class="q-ma-sm" style="border: 1px dashed gainsboro; border-radius: 4px">
               <q-btn
                 class="fit text-center"
                 flat
                 no-caps
                 color="primary"
-                @click="() => (showOfferInput = true)"
+                @click="() => (articleDialog = true)"
                 label="ajouter un article"
                 icon="add"
               >
                 <article-input-dialog
                   :offer="offer"
-                  v-model="showOfferInput"
+                  v-model="articleDialog"
                   @finished="(article: Partial<IArticle>) => handleAddArticle(article)"
                 />
               </q-btn>
-            </q-card>
+            </p>
           </q-tab-panel>
         </q-tab-panels>
-      </template>
-    </q-splitter>
+      </q-card-section>
+    </div>
   </q-card>
 </template>
 
@@ -84,7 +176,9 @@ import ArticleItem from './ArticleItem.vue'
 import ArticleInputDialog from './ArticleInputDialog.vue'
 import { useArticleStore } from '@/stores/article.store'
 
-const showOfferInput = ref(false)
+const offerDialog = ref(false)
+const articleDialog = ref(false)
+
 const props = defineProps<{
   game: IGame
 }>()
@@ -92,7 +186,6 @@ const $offerStore = useOfferStore()
 const $articleStore = useArticleStore()
 
 const offerTab = ref((props.game.offers ?? [])[0]?.id || 'none')
-const splitterModel = ref(20)
 
 const handleAddOffer = async (newOffer: Partial<IOffer>) => {
   await $offerStore.addOffer(newOffer)
@@ -106,6 +199,12 @@ const handleAddArticle = async (newArticle: Partial<IArticle>) => {
 <style scoped lang="css">
 .article {
   width: 100%;
-  max-width: 250px;
+  max-width: 350px;
+}
+
+.image {
+  background-size: cover !important;
+  background-position: top center !important;
+  max-height: 15em;
 }
 </style>
